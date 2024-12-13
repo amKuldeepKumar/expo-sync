95b5514caee2fbb6848ca52676f6782d23de7960
= <=>
2024-12-13 16:57:35 +0530

    auth page fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { Box } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import AppDrawer from "./app/components/AppDrawer";
 import Auth from "./app/features/authentication/Auth";
+import { useEffect } from "react";
+
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const data = localStorage.getItem("User");
+    const user = JSON.parse(data);
+    if (!user) {
+      navigate("/auth");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const isLoggedIn = () => {
     const data = localStorage.getItem("User");
     const user = JSON.parse(data);
